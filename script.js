13f3c8b6bd79564a6c587b6c3285410c4e1b5dd0
Obed Paz <lesterpaz99@gmail.com>
2020-10-29 15:03:42 -0600

    he cambiado todo el codigo de Js para mejor optimizacion

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.querySelector(".main-header__logo").addEventListener("click", function() {
-    document.querySelector(".main-header__menu").classList.toggle("is-active"); 
-});
+const mobile = window.matchMedia("only screen and (max-width: 476px)");
+
+mobile.addEventListener("change", validation);
+
+const menuButton = document.querySelector(".main-header__logo");
+const menu = document.querySelector(".main-header__menu");
+const menuItems = document.querySelectorAll(".menu-item");
+const menuItemsArray = Array.from(menuItems);
+
+validation(mobile);
+
+function openMenu() {
+    menu.classList.toggle("is-active");
+}
+
+function validation(event) {
+    if (event.matches) {
+        menuButton.addEventListener("click", openMenu); 
+        menuItemsArray.forEach(element => {
+            element.addEventListener("click", openMenu);
+        }); 
+    } else {
+        menuButton.removeEventListener("click", openMenu);
+        menuItemsArray.forEach(element => {
+            element.removeEventListener("click", openMenu);
+        });
+    }
+}
